b3b554e13e0000be0d5ef1303516f562d74fed1c
7FFF00 <t42please@gmail.com>
2019-12-10 15:44:22 -0800

    Solve function definition for getElementByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+
+  var findElementClassList = function(element) {
+    if (element.classList && Array.from(element.classList).includes(className)) {
+      result.push(element);
+    } 
+
+    if (element.childNodes) {
+      for (var i = 0; i < element.childNodes.length; i++) {
+        findElementClassList(element.childNodes[i]);
+      }
+    }
+  };
+
+  findElementClassList(document.body);
+
+  return result;
 };
